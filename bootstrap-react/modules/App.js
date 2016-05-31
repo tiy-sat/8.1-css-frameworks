ecd6a8c67f0425ae09bc19d538a1e6a08c789819
Jason Vanderslice <jlvanderslice@gmail.com>
2016-05-31 14:05:48 -0500

    Examples of using css frameworks from lecture

---
+++ bootstrap-react/modules/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Bootstrap from 'bootstrap-sass'
+
+export default React.createClass({
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="dropdown">
+            <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              Dropdown
+              <span className="caret"></span>
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" className="divider"></li>
+              <li><a href="#">Separated link</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
